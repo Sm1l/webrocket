5bf217c9e2b293689a523301c041e695dd3b6ea2
Sm1le <onekonstantin@gmail.com>
2024-04-07 15:59:04 +0300

    stylelint + Nikita's feedback 7.04.24

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import styles from "./page.module.scss";
 import { SingleQuestion } from "@/components/SingleQuestion";
 import { questionsData } from "../../Data/QuestionsData";
+import { Container } from "@/components/Container";
 
 export const metadata: Metadata = {
   title: "ВЕБ рокет | Вопросы",
@@ -9,11 +10,13 @@ export const metadata: Metadata = {
 
 export default function Questions() {
   return (
-    <div className={styles.questions}>
-      <h2 className={styles.title}>Вопросы</h2>
-      {questionsData.map((question) => (
-        <SingleQuestion key={question.id} question={question} />
-      ))}
-    </div>
+    <Container>
+      <div className={styles.questions}>
+        <h2 className={styles.title}>Вопросы</h2>
+        {questionsData.map((question) => (
+          <SingleQuestion key={question.id} question={question} />
+        ))}
+      </div>
+    </Container>
   );
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,22 +1,36 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 
 import { Container } from "../Container";
-import { Logo } from "../Logo";
 import { Social } from "../Social";
 import { NavigationFooter } from "../NavigationFooter";
 
 import styles from "./Footer.module.scss";
+import { screenSizeMore767 } from "@/helpers/screenSizeMore767";
 
 interface FooterProps {}
 
 const Footer: React.FC<FooterProps> = () => {
+  const [isWideScreen, setIsWideScreen] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsWideScreen(screenSizeMore767);
+    };
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <footer className={styles.footer}>
       <Container>
         <div className={styles.footerContainer}>
-          <Logo />
           <NavigationFooter />
-          <Social />
+          {!isWideScreen && <Social />}
         </div>
       </Container>
     </footer>
