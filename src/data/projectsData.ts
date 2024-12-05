66bd2e7f71e1824d2f2152029c9107eecb8d24e0
nikitastepanov <nikitastepanov@feedvisor.com>
2024-12-05 14:26:06 +0300

    Actualized projects list: removed Waves and added Touch

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -1,10 +1,10 @@
 import { nanoid } from "nanoid";
 import { StaticImageData } from "next/image";
 import listsImg from "../../public/images/lists.webp";
-import lofiImg from "../../public/images/lofi.webp";
 import portfolioImg from "../../public/images/portfolio.webp";
 import captureImg from "../../public/images/capture.webp";
 import bazavagaImg from "../../public/images/bazavaga.webp";
+import touchImg from "../../public/images/touch.png";
 
 export type TProjectData = {
   id: string;
@@ -18,18 +18,10 @@ export type TProjectData = {
 export const projectsData: TProjectData[] = [
   {
     id: nanoid(),
-    image: lofiImg,
-    title: "Библиотека Lo-Fi музыки",
-    description: "Лофи музыкальная библиотека",
-    href: "https://lioger.github.io/waves/",
-    features: ["Web Application", "UI/UX", "Design"],
-  },
-  {
-    id: nanoid(),
-    image: portfolioImg,
-    title: "Nikita Stepanov",
-    description: "Портфолио",
-    href: "https://nikitastepanov.vercel.app/",
+    image: touchImg,
+    title: 'Touch',
+    description: 'Студия дизайна',
+    href: "https://sm1l.github.io/Touch/",
     features: ["Landing Page", "UI/UX", "Design"],
   },
   {
@@ -42,9 +34,17 @@ export const projectsData: TProjectData[] = [
   },
   {
     id: nanoid(),
+    image: portfolioImg,
+    title: "Nikita Stepanov",
+    description: "Портфолио",
+    href: "https://nikitastepanov.vercel.app/",
+    features: ["Landing Page", "UI/UX", "Design"],
+  },
+  {
+    id: nanoid(),
     image: captureImg,
     title: "Capture",
-    description: "Создание сайтов",
+    description: "Видео-продакшн",
     href: "https://lioger.github.io/capture/",
     features: ["Landing Page", "UI/UX", "Design"],
   },
@@ -55,5 +55,5 @@ export const projectsData: TProjectData[] = [
     description: "Автосервис автомобилей концерна VAG",
     href: "https://bazavaga.ru",
     features: ["Landing Page", "UI/UX", "Design"],
-  },
+  }
 ];
